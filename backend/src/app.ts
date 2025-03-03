7cff47b25419f61bd44c99b96967ca196784061e
Tushar <tushar@rogtushar>
2025-03-03 21:38:46 +0530

    Initial Commit

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import {config} from "dotenv";
+import morgan from 'morgan';
+config();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));//remove it in production
+
+app.use("api/v1")
+
+export default app;
---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import app from "./app.js";
+import { connectToDatabase } from "./repo/connection.js";
+
+
+// // Routing in express js using node
+// app.get("/hello", (req, res, next) => { return res.send("Hello") });
+// app.post("/hello", (req, res, next) => { return res.send(`Hello ${req.body.name}`) });
+// app.delete("/user/:id", (req, res, ext) => { return res.send(`Hello ${req.params.id}`)});
+
+//connections and listeners
+const PORT = process.env.PORT || 8080;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Server open and connected to database"));
+}).catch(err => console.error(err));
+
+
