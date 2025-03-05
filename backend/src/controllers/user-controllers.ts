8afb9f35f0da252667ed61521c0f2e9a97975bb1
Tushar <tushar@rogtushar>
2025-03-06 01:49:17 +0530

    Added basic User login/signUp funtionality

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,38 +1,64 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        //get all users
-        const users = await User.find();
-        return res.status(200).json({ message: "OK", users });
-    } catch (error) {
-        return res.status(200).json({ meFssage: "ERROR", cause: error.message });
-    }
+  try {
+    //get all users
+    const users = await User.find();
+    return res.status(201).json({ message: "OK", users });
+  } catch (error) {
+    return res.status(200).json({ meFssage: "ERROR", cause: error.message });
+  }
 };
+
 export const userSignup = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        //user signup
-        const { name, email, password } = req.body;
-        console.log(password);
-        const hashedPassword = await hash(password, 10);
-        const user = new User({
-            name: name,
-            email: email,
-            password: hashedPassword,
-        });
-        await user.save();
-        return res.status(200).json({ message: "OK", id: user._id.toString() });
-    } catch (error) {
-        return res.status(200).json({ message: "ERROR", cause: error.message });
-    }
+  try {
+    //user signup
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already registered");
+
+    const hashedPassword = await hash(password, 10);
+    const user = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+    await user.save();
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user login
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send("User not registered");
+
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) return res.status(403).send("Incorrect Password");
+
+    return res.status(201).json({ message: "OK", id: user._id.toString() });
+
+  } catch (error) {
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
 };
