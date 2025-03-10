ba5900bf3a8e274d2b699021d775575165f24ec3
Tushar Rathi <rathitushar2021@gmail.com>
2025-03-10 15:27:21 +0530

    Added routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab, sans serif",
+    allVariants: { color: "white" },
+  },
+});
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
