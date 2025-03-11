2c80c5e98b9ee34ecee6c7cfac9dca12eb4a08ab
Tushar Rathi <rathitushar2021@gmail.com>
2025-03-11 12:33:53 +0530

    Added login/logout state

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,50 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLinks from "./shared/NavigationLinks";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo></Logo>
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLinks
+                bg="#00fffc"
+                to="/chat"
+                text="Go to Chat"
+                textColor="black"
+              />
+              <NavigationLinks
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLinks
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLinks
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Sign In"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
