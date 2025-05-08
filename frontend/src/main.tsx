6968c1ec5a669ced7692705cbc8e15e3daaa487a
Tushar Rathi <rathitushar2021@gmail.com>
2025-05-08 08:57:34 +0530

    Added env variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,9 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = `${
+  import.meta.env.VITE_REACT_APP_BACKEND_BASEURL
+}/api/v1`;
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
